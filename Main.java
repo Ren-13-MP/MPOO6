34e2a242b2dc55a1fefb22844bf91be5dfe28a7f
RenatoMorales1 <92940410+Ren-13-MP@users.noreply.github.com>
2021-10-28 21:09:35 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+/**
+*@author Renato Morales
+*/
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    System.out.println("La suma es: "+suma(2,2));
+  }
+  /**
+  *@param a Primer entero de la ecuacion
+  *@param b Segundo entero de la ecuacion 
+  *@return   La suma de los enteros a mas b
+  */
+  public static int suma(int a, int b){
+    return a+b;
+  }
+
+}
